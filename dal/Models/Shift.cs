fd700dc0e812a165cbe91e1b7491e5be12561feb
Rubab Bashar <rubabbashar67@gmail.com>
2024-04-27 00:19:08 +0600

    security database added

---
+++ dal/Models/Shift.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.Models
+{
+    public class Shift
+    {
+        [Key]
+        public int sid { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string position { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string shift { get; set; }
+
+        [ForeignKey("Security")]
+
+        public int secId { get; set; }
+
+        public virtual Security Security { get; set; }
+    }
+}
