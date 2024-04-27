05345804f7a191e8ae9e6f281b10985fedc0258c
sajid43677 <20-43677-2@student.aiub.edu>
2024-04-28 00:39:55 +0600

    Repair and Tenant model added

---
+++ dal/Models/Tenant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.Models
+{
+    public class Tenant
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public int floorNo { get; set; }
+        [Required]
+        public string flatNo { get; set; }
+        [Required]
+        public int uid { get; set; }
+
+        public virtual ICollection<Repair> Repairs { get; set; }
+        public Tenant()
+        {
+            Repairs = new List<Repair>();
+        }
+    }
+}
---
+++ dal/Repos/TenantRepo.cs
@@ -0,0 +1,47 @@
+﻿using dal.Interfaces;
+using dal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.Repos
+{
+    internal class TenantRepo: Repo, IRepo<Tenant, int, Tenant>
+    {
+        public Tenant Create(Tenant entity)
+        {
+            db.Tenants.Add(entity);
+            db.SaveChanges();
+            return entity;
+        }
+
+        public bool Delete(int id)
+        {
+            var tenant = db.Tenants.Find(id);
+            db.Tenants.Remove(tenant);
+            db.SaveChanges();
+            return true;
+        }
+
+        public Tenant Read(int id)
+        {
+            return db.Tenants.Find(id);
+        }
+
+        public List<Tenant> Read()
+        {
+            return db.Tenants.ToList();
+        }
+
+        public Tenant Update(Tenant entity)
+        {
+            var tenant = db.Tenants.Find(entity.id);
+            tenant.floorNo = entity.floorNo;
+            tenant.flatNo = entity.flatNo;
+            db.SaveChanges();
+            return tenant;
+        }
+    }
+}
